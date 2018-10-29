6479032b77d2cb9ccbec5e23a6082281030b40eb
timelessfb <1282374749@qq.com>
2018-10-29 11:55:19 +0800

    v2

--- simu/test.py
+++ simu/test.py
@@ -4,7 +4,9 @@
 import math
 import numpy as np
 import random
+import matplotlib.pylab as plt
 
-# import simu/GAv2.py
-a=np.size([], 0)
-print(a)
+a = [0.81880408, 1.01824969, 1.29061565, 1.20364224, 1.56843245, 1.85132934,
+     2.66278927, 3.01479499, 4.93524486, 0., 0.]
+b = range(1, 2)
+print(b)
