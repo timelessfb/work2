9c303435b389beba2765baab8b5938109a75a40e
timelessfb <1282374749@qq.com>
2018-12-29 19:48:13 +0800

    .

--- work3/NonlinearMinimize.py
+++ work3/NonlinearMinimize.py
@@ -1,31 +1,93 @@
 #!usr/bin/env python
 # -*- coding:utf-8 -*-
+# import numpy as np
 import numpy as np
+from scipy import stats
 
 
-def slice(J, C, cover=3, delta=0.1, alpha=10):
-    rho1, rho2 = np.random.uniform(0, alpha, 2)
-    Rs = C * (np.random.rand() + delta)
-    Js = np.random.choice(J, cover)
-    return Rs, rho1, rho2, Js
+def slice(lowbound=1, upbound=10):
+    '''
+    :param lowbound: 比例的下界
+    :param upbound: 比例的上界
+    :return:
+    '''
+    rho1, rho2, rho3 = np.random.uniform(lowbound, upbound, 3)
+    sum = rho1 + rho2 + rho3
+    # 归一化
+    rho1 /= sum
+    rho2 /= sum
+    rho3 /= sum
+    return rho1, rho2, rho3
 
 
-def BSs(J, dowm=10, up=10, compute=10):
+def BSs(J, down=10, up=10, compute=10):
     bss = np.zeros((J, 3))
     for i in range(J):
-        bss[i][0] = dowm
+        bss[i][0] = down
         bss[i][1] = up
         bss[i][2] = compute
 
 
-def slices(J, S, C, cover=3, delta=0.1, alpha=10):
-    scs = np.zeros((S, 6))  # Rs, rho1, rho2, Js, i, j
+def slices(S=18, lowbound=1, upbound=10):
+    scs = np.zeros((S, 3))  # rho1, rho2, rho3
     for i in range(S):
-        scs[0], scs[1], scs[2], scs[3] = slice(J, C, cover, delta, alpha)
+        scs[i][0], scs[i][1], scs[i][2] = slice(lowbound, upbound)
     return scs
 
 
+def ZtoX(i, X_map, S, J_num):
+    if i == -1:
+        return -1, -1
+    t = -1
+    for l_x in range(S):
+        for l_y in range(J_num + 1):
+            if X_map[l_x][l_y] == 0:
+                t += 1
+                if i == t:
+                    return l_x, l_y
+
+
+def XtoZ(l_x, l_y, X_map, S, J_num):
+    if X_map[l_x][l_y] != 0:
+        return -1
+    t = -1
+    if l_x == 0:
+        for j in range(l_y + 1):
+            if X_map[0][j] == 0:
+                t += 1
+        return t
+    for i in range(l_x):
+        for j in range(J_num + 1):
+            if X_map[i][j] == 0:
+                t += 1
+    for j in range(l_y + 1):
+        if X_map[l_x][j] == 0:
+            t += 1
+    return t
+
+
+def foo(X_map, I, ROH, S, J_num, load):
+    while True:
+
+
+
 if __name__ == '__main__':
-    print(slices(6, 10, ))
-    Rs, rho1, rho2, Js = slice(6, 3)
-    print(Rs, rho1, rho2, Js)
+    # 初始参数
+    S = 18
+    J_num = 6
+    # 初始位置
+    I = np.zeros(S, dtype=int)
+    # 暂时初始化,后边需要改
+    for i in range(S):
+        I[i] = i % J_num
+
+    X_map = np.random.binomial(1, 0.5, [S, J_num])
+    X_map -= 1
+    X_map = np.c_[X_map, np.zeros(S)]
+
+    # RHO=np.zeros((S,3))
+    for i in range(S):
+        RHO = slices(S)
+    print(RHO)
+
+    # for iter in range(10):
