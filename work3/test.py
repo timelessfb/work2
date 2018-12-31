fbc770871631cb931c9cb71d01d7a9d45c5d1b76
timelessfb <1282374749@qq.com>
2018-12-31 22:59:52 +0800

    .

--- work3/test.py
+++ work3/test.py
@@ -1,41 +1,77 @@
+#!usr/bin/env python
+# -*- coding:utf-8 -*-
+# import numpy as np
 import numpy as np
 from scipy.optimize import minimize
+import copy
 
 
-def objective(x):
-    return x[0] ** 3 - x[1] ** 3 - x[0] * x[1] + 2 * x[0] ** 2
+def fun(type, x):
+    def f1(x):
+        return x[0]-x[1]
+    def f2(x):
+        return x[0]+x[1]
+    if type==1:
+        return f1
+    else:return f2
+
+    a = x[0]
+    b = x[1]
+    if type == 1:
+        return a + b
+    else:
+        return a - b
 
 
-def constraint1(x):
-    return -x[0] ** 2 - x[1] ** 2 + 6.0
+cons = []
+s=[0,1]
+for i in s:
+    cons.append(lambda x: fun(i, x))
+# cons.append(lambda x:fun(1,x))
+# cons.append(lambda x:fun(0,x))
+# f1=lambda x:fun(0,x)
+# f2=lambda x:fun(1,x)
+f1 = cons[0]
+f2 = cons[1]
+x = [1, 2]
+print(f1(x))
+print(f2(x))
+f = lambda x, y: x + y
+print(f(3, 2))
 
+# def generate_constraints_wrong(n_params):
+#     cons = []
+#     for i in range(n_params):
+#         cons.append({'type': 'ineq', 'fun': lambda x:  x[i]})
+#     return tuple(cons)
+#
+# def generate_constraints_wrong2(n_params):
+#     cons = tuple()
+#     for i in range(n_params):
+#         cons += ({'type': 'ineq', 'fun': lambda x:  x[i]},)
+#     return cons
 
-def constraint2(x):
-    return 2.0 - x[0] * x[1]
-
-
-# initial guesses
-n = 2
-x0 = np.zeros(n)
-x0[0] = 1
-x0[1] = 2
-
-# show initial objective
-print('Initial SSE Objective: ' + str(objective(x0)))
-
-# optimize
-b = (0.0, 3.0)
-bnds = (b, b)
-con1 = {'type': 'ineq', 'fun': constraint1}
-con2 = {'type': 'eq', 'fun': constraint2}
-cons = ([con1, con2])
-solution = minimize(objective, x0, method='SLSQP', \
-                    bounds=bnds, constraints=cons)
-x = solution.x
-
-# show final objective
-print('Final SSE Objective: ' + str(objective(x)))
-# print solution
-print('Solution')
-print('x1 = ' + str(x[0]))
-print('x2 = ' + str(x[1]))
+# def generate_constraints_right(n_params):
+#     # let's create a function generator that uses closure to pass i to the generated function
+#     def wrapper_fun(x, i):
+#         def fun(x):
+#             return x[i]
+#         return fun
+#     cons = []
+#     for i in range(n_params):
+#         f = wrapper_fun(x, i)
+#         cons.append({'type': 'ineq', 'fun': f})
+#     return tuple(cons)
+#
+# # verify the generated functions
+# n_params = 3
+# x = [1,10, 100]
+# cons1 = generate_constraints_wrong(n_params)
+# cons2 = generate_constraints_wrong2(n_params)
+# cons3 = generate_constraints_right(n_params)
+# print(cons1[0]['fun'](x)) # this should be 1 but instead we end up modifying all of our lambda objects to have the last i
+# print(cons1[1]['fun'](x))
+# print(cons2[0]['fun'](x))
+# print(cons2[1]['fun'](x))
+# print(cons3[0]['fun'](x))
+# print(cons3[1]['fun'](x))
